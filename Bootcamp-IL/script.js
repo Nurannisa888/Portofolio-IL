7a680367f937464b525d1df0b542a3b6e9335f0b
Nur Annisa <128034036+Nurannisa888@users.noreply.github.com>
2024-03-05 21:39:07 +0700

    Add files via upload

---
+++ Bootcamp-IL/script.js
@@ -0,0 +1,13 @@
+let text = document.querySelector(".text");
+
+let typed = new Typed(text, {
+  strings: [
+    "Full Stack Developer",
+    "Front End Development",
+    "Back End Developer",
+  ],
+  speed: 50,
+  backSpeed: 30,
+  backDelay: 1000,
+  loop: true,
+});
